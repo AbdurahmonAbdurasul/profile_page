eeab41677d82e378487f3cc5729c032f927a76c1
AbdurahmonAbdurasul <abdurahmon22084@gmail.com>
2024-12-13 11:33:44 +0000

    bottombar

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -7,7 +8,7 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 26),
+        padding: const EdgeInsets.symmetric(horizontal: 26),
         width: double.infinity,
         height: double.infinity,
         decoration: const BoxDecoration(
@@ -37,33 +38,45 @@ class ProfilePage extends StatelessWidget {
                     fontWeight: FontWeight.bold),
               ),
               actions: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.exit_to_app),
-                  color: Colors.white,
-                ),
+                Container(
+                  width: 35,
+                  height: 35,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xFF191C2D)),
+                  child: SvgPicture.asset(
+                    "assets/icons/logout.svg",
+                    width: 20,
+                    height: 20,
+                  ),
+                )
               ],
             ),
-            SizedBox(
-              height: 15,
+            const SizedBox(
+              height: 16,
             ),
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 22),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 22),
               alignment: Alignment.center,
               height: 102,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: const Color(0xFF0D0F18),
+              ),
               child: Row(
                 //mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
+                  const Expanded(
                     child: CircleAvatar(
                       backgroundImage: AssetImage("assets/images/image.png"),
                       radius: 29,
                     ),
                   ),
-                  SizedBox(
-                    width: 15,
+                  const SizedBox(
+                    width: 16,
                   ),
-                  Expanded(
+                  const Expanded(
                     flex: 4,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -95,7 +108,7 @@ class ProfilePage extends StatelessWidget {
                           alignment: Alignment.topCenter,
                           width: 51,
                           height: 21,
-                          decoration: BoxDecoration(
+                          decoration: const BoxDecoration(
                             color: Color(0xFF262938),
                           ),
                         ),
@@ -104,97 +117,163 @@ class ProfilePage extends StatelessWidget {
                   ),
                 ],
               ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Color(0xFF0D0F18),
-              ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             ListTile(
-              leading: Icon(
-                Icons.edit,
-                color: Colors.white,
-                size: 30,
+              leading: SvgPicture.asset(
+                "assets/icons/pencil.svg",
+                width: 30,
+                height: 30,
               ),
               title: const Text(
                 "Edit Profile",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
-                  fontWeight: FontWeight.w400,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
             ),
+            const Divider(
+              height: 2,
+              thickness: 2,
+              color: Color(0xFF212433),
+            ),
             ListTile(
-              leading: Icon(
-                Icons.location_on,
-                color: Colors.white,
-                size: 30,
+              leading: SvgPicture.asset(
+                "assets/icons/location_arrow.svg",
+                height: 30,
+                width: 30,
               ),
               title: const Text(
                 "Ofis map",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
-                  fontWeight: FontWeight.w400,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
             ),
+            const Divider(
+              height: 2,
+              thickness: 2,
+              color: Color(0xFF212433),
+            ),
             ListTile(
-              leading: Icon(
-                Icons.headphones,
-                color: Colors.white,
-                size: 30,
+              leading: SvgPicture.asset(
+                "assets/icons/headset.svg",
+                width: 30,
+                height: 30,
               ),
               title: const Text(
                 "Qo'llab quvvatlash",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
-                  fontWeight: FontWeight.w400,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
             ),
+            const Divider(
+              height: 2,
+              thickness: 2,
+              color: Color(0xFF212433),
+            ),
             ListTile(
-              leading: Icon(
-                Icons.book,
-                color: Colors.white,
-                size: 30,
+              leading: SvgPicture.asset(
+                "assets/icons/list.svg",
+                width: 30,
+                height: 30,
               ),
               title: const Text(
                 "Biz haqimizda",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
-                  fontWeight: FontWeight.w400,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
             ),
+            const Divider(
+              height: 2,
+              thickness: 2,
+              color: Color(0xFF212433),
+            ),
             ListTile(
-              leading: Icon(
-                Icons.sunny,
-                color: Colors.white,
-                size: 30,
+              leading: SvgPicture.asset(
+                "assets/icons/sun.svg",
+                width: 30,
+                height: 30,
               ),
               title: const Text(
                 "Dark theme",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
-                  fontWeight: FontWeight.w400,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
               trailing: Switch(
                 value: true,
                 activeColor: Colors.white,
-                activeTrackColor: Colors.blue,
+                activeTrackColor: const Color(0xFF0085FF),
                 onChanged: (value) {},
               ),
-            )
+            ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF151825),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/homee.svg",
+              width: 26,
+              height: 26,
+            ),
+            label: "Bosh sahifa",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/candlesticks.svg",
+              width: 26,
+              height: 26,
+            ),
+            label: "Bosh sahifa",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/invoice.svg",
+              width: 26,
+              height: 26,
+            ),
+            label: "Ro'yhat",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/fire.svg",
+              width: 26,
+              height: 26,
+            ),
+            label: "Olov",
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/user.svg",
+              width: 26,
+              height: 26,
+            ),
+            label: "Person",
+          ),
+        ],
       ),
     );
   }
